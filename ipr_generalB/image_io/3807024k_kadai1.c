787ca6dd120cf8711a33825d7463a19dbb948687
k-nabe <58263530+shirom0@users.noreply.github.com>
2024-04-16 15:59:10 +0900

    Add files via upload

---
+++ ipr_generalB/image_io/3807024k_kadai1.c
@@ -0,0 +1,192 @@
+//3807024k 鍋島虎太郎 必須課題1
+/* 画像にネガポジ変換を行い，出力するプログラム*/
+/* load_PGM,save_PGMでモノクロ画像，load_PPM,save_PPMでカラー画像を処理する*/
+/*モノクロ画像はgray_np.pgm，カラー画像はcolor_np.ppm で出力される*/
+/* 実行環境: gcc 9.2.0*/
+
+/*コマンドライン引数から，画像名.ppm(画像名.pgm)が入力されることを想定*/
+
+#include <stdio.h>
+#include <stdlib.h>     /* 異常終了 exit() で必要 */
+#include <string.h>     /*拡張子判定*/
+
+#define HEIGHT 256     /* 縦画素数 */
+#define WIDTH  256     /* 横画素数 */
+#define mode 2         /* PPMかPGMか指定*/
+
+/* 関数のプロトタイプ宣言 */
+void load_PGM(unsigned char image[][WIDTH], const char *filename);
+void save_PGM(unsigned char image[][WIDTH], const char *filename);
+
+void load_PPM(unsigned char image[][WIDTH][3], const char *filename);
+void save_PPM(unsigned char image[][WIDTH][3], const char *filename);
+
+int main(int argc, char *argv[])
+{
+    
+    if(mode==1){    //mode=1:ppm
+        unsigned char src_image[HEIGHT][WIDTH][3]; 
+        load_PPM(src_image, argv[1]);
+        save_PPM(src_image, argv[1]);
+    }
+
+    if(mode==2){     //mode=2:pgm
+        unsigned char src_image[HEIGHT][WIDTH]; 
+        load_PGM(src_image, argv[1]);
+        save_PGM(src_image, argv[1]); 
+    }
+
+    return 0;
+}
+
+/* 関数load_PGM: 二次元配列image に格納*/
+void load_PGM(unsigned char image[][WIDTH], const char *filename){
+
+    char magic_number[2];
+    int height, width;
+    int max_intensity;
+    FILE *fp;
+
+    fp = fopen(filename, "rb");
+    if (fp == NULL) {
+        fprintf(stderr, "画像 %s が開けませんでした．\n", filename);
+        exit(1);
+    }
+
+    /* マジックナンバを読込んでチェック */
+    fscanf(fp, "%c%c", &magic_number[0], &magic_number[1]);
+    if (magic_number[0] != 'P' || magic_number[1] != '5') {
+        fprintf(stderr, "画像 %s はバイナリ形式の PGM ではありません．", filename);
+	exit(1);
+    }
+
+    /* 画像のサイズを読込んでチェック */
+    fscanf(fp, "%d %d", &width, &height);
+    if (width != WIDTH || height != HEIGHT) {
+        fprintf(stderr, "サイズが異なります．\n");
+	exit(1);
+    }
+
+    /* 最大階調値を読込んでチェック */
+    fscanf(fp, "%d", &max_intensity);
+    if (max_intensity != 255) {
+        fprintf(stderr, "最大階調値が範囲外です．\n");
+        exit(1);
+    }
+    fgetc(fp); /* ヘッダと画像データの区切りである改行文字の読み飛ばし */
+
+    /* 画像データの読込み */
+    fread(image, sizeof(unsigned char), HEIGHT * WIDTH, fp);
+
+    fclose(fp);
+}
+
+/* save_PGM: 入力画像のネガポジ変換を行い，pgmファイルを保存する*/
+void save_PGM(unsigned char image[][WIDTH], const char *filename){
+
+    char magic_number[2];
+    int height, width;
+    int max_intensity;
+    FILE *fp;
+
+    /* ヘッダ情報の読み出し*/
+    fp = fopen(filename, "rb");
+
+    fscanf(fp, "%c%c", &magic_number[0], &magic_number[1]);
+    fscanf(fp, "%d %d", &width, &height);
+    fscanf(fp, "%d", &max_intensity);
+
+    /* ネガポジ変換*/
+    for(int h=0;h<HEIGHT;h++){
+        for(int w=0;w<WIDTH;w++){
+            image[h][w] = 255 - image[h][w];
+        }
+    }
+
+    /* ヘッダ情報の書き出し*/
+    fp = fopen("gray_np.pgm", "wb");
+
+    fprintf(fp, "%c%c\n", magic_number[0], magic_number[1]); 
+    fprintf(fp, "%d %d\n", width, height);
+    fprintf(fp, "%d\n", max_intensity);
+    fwrite(image, sizeof(unsigned char), HEIGHT * WIDTH, fp); /* 画像データの書き出し */
+    fclose(fp); 
+
+}
+
+/* 関数load_PPM: 三次元配列image に格納*/
+void load_PPM(unsigned char image[][WIDTH][3], const char *filename){
+
+    char magic_number[2];
+    int height, width;
+    int max_intensity;
+    FILE *fp;
+
+    fp = fopen(filename, "rb");
+    if (fp == NULL) {
+        fprintf(stderr, "画像 %s が開けませんでした．\n", filename);
+        exit(1);
+    }
+
+    /* マジックナンバを読込んでチェック */
+    fscanf(fp, "%c%c", &magic_number[0], &magic_number[1]);
+    if (magic_number[0] != 'P' || magic_number[1] != '6') {
+        fprintf(stderr, "画像 %s はバイナリ形式の PPM ではありません．", filename);
+	exit(1);
+    }
+
+    /* 画像のサイズを読込んでチェック */
+    fscanf(fp, "%d %d", &width, &height);
+    if (width != WIDTH || height != HEIGHT) {
+        fprintf(stderr, "サイズが異なります．\n");
+	exit(1);
+    }
+
+    /* 最大階調値を読込んでチェック */
+    fscanf(fp, "%d", &max_intensity);
+    if (max_intensity != 255) {
+        fprintf(stderr, "最大階調値が範囲外です．\n");
+        exit(1);
+    }
+    fgetc(fp); /* ヘッダと画像データの区切りである改行文字の読み飛ばし */
+
+    /* 画像データの読込み */
+    fread(image, sizeof(unsigned char), HEIGHT * WIDTH * 3, fp);
+
+    fclose(fp);
+}
+
+/* save_PPM: 入力画像のネガポジ変換を行い，ppmファイルを保存する*/
+void save_PPM(unsigned char image[][WIDTH][3], const char *filename){
+
+    char magic_number[2];
+    int height, width;
+    int max_intensity;
+    FILE *fp;
+
+    /* ヘッダ情報の読み出し*/
+    fp = fopen(filename, "rb");
+
+    fscanf(fp, "%c%c", &magic_number[0], &magic_number[1]);
+    fscanf(fp, "%d %d", &width, &height);
+    fscanf(fp, "%d", &max_intensity);
+
+    /* ネガポジ変換*/
+    for(int h=0;h<HEIGHT;h++){
+        for(int w=0;w<WIDTH;w++){
+            for (int rgb_num=0;rgb_num<3;rgb_num++){
+                image[h][w][rgb_num] = 255 - image[h][w][rgb_num];
+            }
+        }
+    }
+
+    /* ヘッダ情報の書き出し*/
+    fp = fopen("color_np.ppm", "wb");
+
+    fprintf(fp, "%c%c\n", magic_number[0], magic_number[1]); 
+    fprintf(fp, "%d %d\n", width, height);
+    fprintf(fp, "%d\n", max_intensity);
+    fwrite(image, sizeof(unsigned char), HEIGHT * WIDTH*3, fp); /* 画像データの書き出し */
+    fclose(fp); 
+
+}
---
+++ ipr_generalB/image_io/3807024k_kadai2.cpp
@@ -0,0 +1,99 @@
+﻿//3807024k 鍋島虎太郎 必須課題2
+
+/* 画像のネガポジ変換を行うプログラム*/
+/*二重ループとbitwise_notを使用した変換を行い，時間を計測*/
+/*実行環境：visual studio 2022(community)，opencv 4.7.0*/
+
+/*コマンドライン引数より，画像名.png が入力されることを想定する*/
+
+#include <iostream>
+#include <chrono>
+#include <opencv2/opencv.hpp>
+
+// OpenCV のバージョンが違うなら名前は 454 の部分を自身のものに変更する
+#ifdef _DEBUG
+#pragma comment(lib, "opencv_world470d.lib")
+#else
+#pragma comment(lib, "opencv_world470.lib")
+#endif
+
+int main(int argc, char* argv[])
+{
+
+    // 画像パス
+    cv::Mat img = cv::imread(argv[1]);
+
+    //変換後の画像
+    cv::Mat img_np_loop;
+    cv::Mat img_np_bwn;
+
+    //chronoによる時間計測
+    std::chrono::system_clock::time_point  start, end;
+    std::time_t time_stamp;
+
+    //二重ループによるネガポジ変換
+    start = std::chrono::system_clock::now();   //時間計測開始
+    if (img.channels() == 3) {
+        //変換後の画像img_npを初期化
+        img_np_loop = cv::Mat::zeros(img.rows, img.cols, CV_8UC3);   //CV_8UC3: 1個のCV_8U モノクロ画像の初期値
+
+        //二重ループでネガポジ変換
+        for (int i = 0; i < img.rows; i++) {
+            for (int j = 0; j < img.cols; j++) {
+                img_np_loop.at<cv::Vec3b>(i, j)[0] = 255 - img.at<cv::Vec3b>(i, j)[0];   //B
+                img_np_loop.at<cv::Vec3b>(i, j)[1] = 255 - img.at<cv::Vec3b>(i, j)[1];   //G
+                img_np_loop.at<cv::Vec3b>(i, j)[2] = 255 - img.at<cv::Vec3b>(i, j)[2];   //R
+            }
+        }
+    }
+    else if (img.channels() == 1) {
+        //変換後の画像img_npを初期化
+        img_np_loop = cv::Mat::zeros(img.rows, img.cols, CV_8UC1);  //CV_8UC1: 1個のCV_8U モノクロ画像の初期値
+
+        //二重ループでネガポジ変換
+        for (int i = 0; i < img.rows; i++) {
+            for (int j = 0; j < img.cols; j++) {
+                img_np_loop.at<uchar>(i, j) = 255 - img.at<uchar>(i, j);
+            }
+        }
+    }
+    end = std::chrono::system_clock::now(); //時間計測終了
+    auto time_loop = end - start;
+
+    // 処理に要した時間をミリ秒に変換
+    auto msec_loop = std::chrono::duration_cast<std::chrono::milliseconds>(time_loop).count();
+    std::cout << "use_loop: " << msec_loop << " msec" << std::endl;  //bitwise_notの処理時間
+
+    //bitwise_notによるネガポジ変換
+    start = std::chrono::system_clock::now();   //時間計測開始
+    if (img.channels() == 3) {
+        //変換後の画像img_npを初期化
+        img_np_bwn = cv::Mat::zeros(img.rows, img.cols, CV_8UC3);
+
+        cv::bitwise_not(img, img_np_bwn);
+
+    }else if (img.channels() == 1) {
+        //変換後の画像img_npを初期化
+        img_np_bwn = cv::Mat::zeros(img.rows, img.cols, CV_8UC1);
+
+        cv::bitwise_not(img, img_np_bwn);
+    }
+
+    end = std::chrono::system_clock::now(); //時間計測終了
+    auto time_bwn = end - start;
+
+    // 処理に要した時間をミリ秒に変換
+    auto msec_bwn = std::chrono::duration_cast<std::chrono::milliseconds>(time_bwn).count();
+    std::cout << "use_bitwise_not: " << msec_bwn << " msec" << std::endl;   //bitwise_notの処理時間
+
+
+    //画像の出力
+    cv::imwrite("loop_np.png", img_np_loop);
+    cv::imwrite("bitwise_not_np.png", img_np_bwn);
+
+    cv::imshow("use_loop", img_np_loop);
+    cv::imshow("use_bitwise_not", img_np_bwn);
+    cv::waitKey(0);
+
+    return 0;
+}
